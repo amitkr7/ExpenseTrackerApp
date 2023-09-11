fc9b032d3cd01a710b303447ef4bd34c884f82c9
amitkr7 <amit840901@gmail.com>
2023-09-11 20:09:24 +0530

    Adding and Configuring Navigation

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const RecentExpense = () => {
+const RecentExpenses = () => {
   return <Text>RecentExpense</Text>;
 };
 
-export default RecentExpense;
+export default RecentExpenses;
