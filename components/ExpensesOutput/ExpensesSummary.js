92f210cf568d606fe51a062b7839a26143372797
amitkr7 <amit840901@gmail.com>
2023-09-12 21:05:53 +0530

    Creating component to dispaly expense

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ExpensesSummary = () => {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>Amount</Text>
+    </View>
+  );
+};
+
+export default ExpensesSummary;
