daa60e8040bfbec59f637a07e961161509799aba
amitkr7 <amit840901@gmail.com>
2023-09-13 20:13:28 +0530

    Adding dummy expense data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,43 @@ import { View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
-const ExpensesOutput = ({ expenses }) => {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'Books',
+    amount: 50,
+    date: new Date('2021-12-19'),
+  },
+  {
+    id: 'e2',
+    description: 'Vegetables',
+    amount: 40,
+    date: new Date('2022-10-09'),
+  },
+  {
+    id: 'e3',
+    description: 'FRUITS',
+    amount: 40,
+    date: new Date('2023-10-09'),
+  },
+  {
+    id: 'e4',
+    description: 'CLOTHES',
+    amount: 49,
+    date: new Date('2023-02-08'),
+  },
+  {
+    id: 'e5',
+    description: 'SHOES',
+    amount: 70,
+    date: new Date('2022-12-12'),
+  },
+];
+
+const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
 const AllExpenses = () => {
-  return <Text>AllExpenses</Text>;
+  return <ExpensesOutput expensesPeriod='Last & days' />;
 };
 
 export default AllExpenses;
