7091999bf3b407c1d827a63f7a119374e2f4b971
amitkr7 <amit840901@gmail.com>
2023-09-13 20:37:33 +0530

    Improving App styling and Layout

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -3,7 +3,7 @@ import { Text } from 'react-native';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
 const AllExpenses = () => {
-  return <ExpensesOutput expensesPeriod='Last & days' />;
+  return <ExpensesOutput expensesPeriod='Last 7 days' />;
 };
 
 export default AllExpenses;
